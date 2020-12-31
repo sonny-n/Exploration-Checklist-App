2cf97ec7332ac3f353def3eb7528975a5b83d65c
Son Nguyen <48631696+sonny-n@users.noreply.github.com>
2020-12-30 23:39:55 -0500

    Initial Commit

---
+++ Exploration checklist/Exploration_checklistApp.swift
@@ -0,0 +1,17 @@
+//
+//  Exploration_checklistApp.swift
+//  Exploration checklist
+//
+//  Created by Son Nguyen on 12/30/20.
+//
+
+import SwiftUI
+
+@main
+struct Exploration_checklistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
